65bb08a53e58d448bfd21781d3703ef508b85fcd
samaun <samaunkhan77@gmail.com>
2024-04-12 22:28:59 +0600

    Project 1

---
+++ Calculator/calculator.js
@@ -0,0 +1,31 @@
+document.getElementById('calculator-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operator = document.getElementById('operator').value;
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = "Cannot divide by zero";
+            }
+            break;
+        default:
+            result = "Invalid operator";
+    }
+
+    document.getElementById('result').innerText = `Result: ${result}`;
+});
